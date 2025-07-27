80394300a8583f20fe8eb33e426195609447faae
gabrielceh <gabrielcehu@gmail.com>
2025-07-27 15:40:32 -0500

    fin de la seccion

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,27 +1,62 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes:[
-    GoRoute(
-      path: '/',
-      name: HomeScreen.routeName,
-      builder: (context, state) => const HomeScreen(),
-      // rutas hijas o deep links
+
+    /**Rutas padre-hijo */
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.routeName,
+    //   builder: (context, state) => HomeScreen(childView: HomeView()),
+    //   // rutas hijas o deep links
+    //   routes: [
+    //     GoRoute(
+    //       path: 'movie/:movieId',
+    //       name: MovieScreen.routeName,
+    //       builder: (context, state){
+    //         final movieId = state.pathParameters['movieId'] ?? 'no-id';
+    //         return MovieScreen(movieId: movieId);
+    //       }
+    //       ,
+    //     )
+
+    //   ]
+    // ),
+
+    ShellRoute(
+      builder: (context, state, child){
+        return HomeScreen(childView: child,);
+      },
+
       routes: [
         GoRoute(
-          path: 'movie/:movieId',
-          name: MovieScreen.routeName,
+          path: '/',
           builder: (context, state){
-            final movieId = state.pathParameters['movieId'] ?? 'no-id';
-            return MovieScreen(movieId: movieId);
+            return HomeView();
+          },
+          routes: [
+             GoRoute(
+              path: 'movie/:movieId',
+              name: MovieScreen.routeName,
+              builder: (context, state){
+                final movieId = state.pathParameters['movieId'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            )
+          ]
+        ),
+        
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state){
+            return FavoritesView();
           }
-          ,
-        )
-
+        ),
       ]
-    ),
+    )
 
   ]
 );
--- lib/presentation/widgets/shared/custom_bottom_navbar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navbar.dart
@@ -1,26 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavbar extends StatelessWidget {
+
+  static int getCurrentIndex(BuildContext context){
+    final String location = GoRouterState.of(context).uri.toString();
+    switch(location){
+      case '/':
+       return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+    
+  }
   
 
   const CustomBottomNavbar({super.key});
 
+  void _onItemTapped (BuildContext context, int index) {
+    switch(index){
+      case 0:
+        // home
+        context.go('/');
+        break;
+      case 1:
+        // categorias
+        break;
+      case 2:
+        // favoritos
+        context.push('/favorites');
+        break;
+      default:
+        break;
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      elevation: 0,    
+      elevation: 0,
+      currentIndex: getCurrentIndex(context),
+        // value es el indice del item que se ha pulsado
+      onTap: (value) => _onItemTapped(context, value),    
       items:const[
         // necesita mas de un item
         BottomNavigationBarItem(
+          activeIcon: Icon(Icons.home_max),
           icon: Icon(Icons.home_max_outlined),
           label: 'Home',
+
         ),
         BottomNavigationBarItem(
+          activeIcon: Icon(Icons.label),
           icon: Icon(Icons.label_outline),
           label: 'Categorias',
         ),
       
         BottomNavigationBarItem(
+          activeIcon: Icon(Icons.favorite),
           icon: Icon(Icons.favorite_outline),
           label: 'Favoritos',
         ),
